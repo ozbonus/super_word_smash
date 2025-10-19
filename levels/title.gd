683c54209588eb929c64eab4dd13207872affca9
Christopher Perry <ozbonus@gmail.com>
2025-10-19 20:35:39 +0800

    Allow selecting games of different lengths.

--- levels/title.gd
+++ levels/title.gd
@@ -1,11 +1,17 @@
+## The exact durations of the various game lengths are set in the TimerService.
 extends Node2D
 
-signal start_game
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+signal start_game(length: Constants.GameLength)
 
 
-func _on_button_pressed():
-	start_game.emit()
+func _on_short_game_button_pressed():
+	start_game.emit(Constants.GameLength.SHORT)
+
+
+func _on_medium_game_button_pressed():
+	start_game.emit(Constants.GameLength.MEDIUM)
+
+
+func _on_long_game_button_pressed():
+	start_game.emit(Constants.GameLength.LONG)
