542e8cd1d51f89a7773e810f7df69c81cfdda670
Christopher Perry <ozbonus@gmail.com>
2025-10-29 03:03:49 +0800

    Greatly simplify how goals are handled.

--- components/goal.gd
+++ components/goal.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Goal extends Node2D
 
 signal entered
@@ -8,11 +9,27 @@ enum GoalType { UNSET, REAL, DECOY }
 ## goal. This setting is required or else and error will be thrown.
 @export var goal_type: GoalType
 
-func _ready():
+## The word that will appear in the game and which is intended to match the ball
+## of that level. Fingers crossed that it updates automatically in the editor.
+@export var word: String:
+	set(value):
+		word = value
+		$Label.text = word
+
+func _get_configuration_warnings():
+	var warnings = []
 	if goal_type == GoalType.UNSET:
-		push_error("You must define a goal type.")
-		return
+		warnings.append("You must set a goal type.")
+	if not has_node("Area2D"):
+		warnings.append("This node requires an Area2D child node.")
+	if not has_node("Label"):
+		warnings.append("This node requires a Label child node.")
+	return warnings
 
 
-func _on_area_2d_body_entered():
-	entered.emit()
+func _ready():
+	pass
+
+func _on_area_2d_body_entered(_body: Node2D):
+	if goal_type == GoalType.REAL:
+		entered.emit()
