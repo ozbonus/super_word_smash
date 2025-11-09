7cc267f28b5c9cf9970d60e28783c8bcfa01f44d
Christopher Perry <ozbonus@gmail.com>
2025-11-09 23:39:44 +0800

    Disable hit sound for now.

--- components/balls/ball.gd
+++ components/balls/ball.gd
@@ -1,9 +1,6 @@
 class_name Ball
 extends RigidBody2D
 
-const IMPACT_THRESHOLD_MAGNITUDE := 20.0
-var previous_velocity_magnitude := 0.0
-
 func _ready():
 	# This might slightly increase responsiveness when touch and motion input
 	# are happening simultaneously.
@@ -17,10 +14,3 @@ func _physics_process(_delta):
 		var adjusted_tilt := tilt_2d.normalized() * (tilt_2d.length() - Constants.DEAD_ZONE)
 		var force := adjusted_tilt * Constants.TILT_SENSITIVITY
 		apply_central_force(force)
-
-	# Check for high speed impacts with physics objects and play a sound if so.
-	if previous_velocity_magnitude > IMPACT_THRESHOLD_MAGNITUDE and linear_velocity.length() < IMPACT_THRESHOLD_MAGNITUDE:
-		print_debug(get_contact_count())
-		SoundEffectsService.play_hit()
-	
-	previous_velocity_magnitude = linear_velocity.length()
