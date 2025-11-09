63b812c0eff7f1ce8e6c1200718aa9f2a84982fb
Christopher Perry <ozbonus@gmail.com>
2025-11-09 18:41:35 +0800

    Write functions to play sounds.

--- services/sound_effects_service.gd
+++ services/sound_effects_service.gd
@@ -1,11 +1,14 @@
 extends Node
 
+@onready var hit: AudioStreamPlayer = $Hit
+@onready var success: AudioStreamPlayer = $Success
+@onready var decoy: AudioStreamPlayer = $Decoy
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func play_hit():
+	hit.play()
 
+func play_success():
+	success.play()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func play_decoy():
+	decoy.play()
