9751af25d325ecfbd98cb25f1c17d169385826ed
Christopher Perry <ozbonus@gmail.com>
2025-11-12 17:57:21 +0800

    Rename to game over message.

--- components/game_over_message.gd
+++ components/game_over_message.gd
@@ -1,6 +1,9 @@
 class_name TimeUpMessage
 extends Control
 
+const TIMES_UP_MESSAGE = "TIME'S UP!"
+const PERFECT_MESSAGE = "PERFECT!"
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func appear() -> void:
