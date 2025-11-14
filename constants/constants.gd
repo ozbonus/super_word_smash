8e8c3c0bf41db621a9437a02e7c2f0c0aab1c005
Christopher Perry <ozbonus@gmail.com>
2025-11-15 01:04:26 +0800

    Add WARMUP and PERFECT game states, along with comments.

--- constants/constants.gd
+++ constants/constants.gd
@@ -1,9 +1,17 @@
 extends Node
 
 
-enum GameLength { SHORT, MEDIUM, LONG }
-enum GameState { TITLE, PLAYING, SUCCESS, TIMEUP, FINISHED }
-const TILT_SENSITIVITY: float =  400.0
-const DEAD_ZONE: float =  1.0
+enum GameLength {SHORT, MEDIUM, LONG}
+enum GameState {
+  TITLE, # Only when the title screen is being shown.
+  WARMUP, # During the countdown before play begins.
+  PLAYING, # During normal game play.
+  SUCCESS, # When getting a ball onto a valid target.
+  TIMEUP, # When time runs out and game play must stop.
+  PERFECT, # When all levels are complete and game play must stop.
+  FINISHED, # When showing the score after finishing the game.
+}
+const TILT_SENSITIVITY: float = 400.0
+const DEAD_ZONE: float = 1.0
 const TRANSITION_DURATION_SECONDS: float = 2.0
 const SUCCESS_TIME_SCALE: float = 0.05
