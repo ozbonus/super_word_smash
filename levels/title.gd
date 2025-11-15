81d96cb6616370782c428619d0f5891f6569b1b1
Christopher Perry <ozbonus@gmail.com>
2025-11-15 19:33:16 +0800

    Connect button and lable to settings service.

--- levels/title.gd
+++ levels/title.gd
@@ -13,6 +13,7 @@ func _ready():
 	game_time_seconds.text = "%d" % SettingsService.game_length
 	mute_toggle.button_pressed = SettingsService.mute_audio
 
+
 func _on_short_game_button_pressed():
 	start_game.emit(Constants.GameLength.SHORT)
 
@@ -28,3 +29,15 @@ func _on_long_game_button_pressed():
 func _on_mute_toggle_toggled(toggled_on: bool):
 	SettingsService.mute_audio = toggled_on
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), toggled_on)
+
+
+func _on_more_time_button_pressed():
+	if SettingsService.game_length < SettingsService.LENGTH_MAX:
+		SettingsService.game_length += SettingsService.LENGTH_STEP
+		game_time_seconds.text = "%d" % SettingsService.game_length
+
+
+func _on_less_time_button_pressed():
+	if SettingsService.game_length > SettingsService.LENGTH_MIN:
+		SettingsService.game_length -= SettingsService.LENGTH_STEP
+		game_time_seconds.text = "%d" % SettingsService.game_length
