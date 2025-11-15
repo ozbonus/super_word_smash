6d652535f22c129261942d6d60b739b605cb2eb1
Christopher Perry <ozbonus@gmail.com>
2025-11-15 19:32:54 +0800

    Make all length range parameters constants.

--- services/settings_service.gd
+++ services/settings_service.gd
@@ -2,8 +2,10 @@
 
 extends Node
 
-const GAME_LENGTH_STEP = 10.0
+const LENGTH_MIN = 10.0
+const LENGTH_MAX = 300.0
+const LENGTH_STEP = 10.0
 
 @export_group("Default settings")
 @export var mute_audio := true
-@export_range(10.0, 300.0, GAME_LENGTH_STEP, "suffix:seconds") var game_length := 60.0
+@export_range(LENGTH_MIN, LENGTH_MAX, LENGTH_STEP, "suffix:seconds") var game_length := 60.0
