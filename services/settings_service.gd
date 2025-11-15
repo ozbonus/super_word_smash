0431494c22f043c2bb00b5ad047fe2b2dc65f6a6
Christopher Perry <ozbonus@gmail.com>
2025-11-15 19:27:47 +0800

    Make game length step size a constant.

--- services/settings_service.gd
+++ services/settings_service.gd
@@ -2,6 +2,8 @@
 
 extends Node
 
+const GAME_LENGTH_STEP = 10.0
+
 @export_group("Default settings")
 @export var mute_audio := true
-@export_range(10.0, 300.0, 10.0, "suffix:seconds") var game_length := 60.0
+@export_range(10.0, 300.0, GAME_LENGTH_STEP, "suffix:seconds") var game_length := 60.0
