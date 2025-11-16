9134215e51e6ae0d9b5413977f99e11c063ce32e
Christopher Perry <ozbonus@gmail.com>
2025-11-17 00:18:45 +0800

    Create a hidden operator menu for use during game play.

---
+++ components/operator_controls.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+signal next_level
+signal restart
+
+@onready var open_button: Button = %OpenButton
+@onready var button_container: CenterContainer = %ButtonContainer
+@onready var next_level_button: Button = %NextLevelButton
+@onready var restart_button: Button = %RestartButton
+@onready var close_button: Button = %CloseButton
+
+
+func _ready():
+	button_container.visible = false
+
+
+func _on_open_button_pressed():
+	button_container.visible = true
+
+
+func _on_next_level_button_pressed():
+	next_level.emit()
+
+
+func _on_restart_button_pressed():
+	restart.emit()
+
+
+func _on_close_button_pressed():
+	button_container.visible = false
