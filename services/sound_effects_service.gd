df5745c3574a47ac5f85465f8adabd73d250c9ea
Christopher Perry <ozbonus@gmail.com>
2025-11-16 19:32:33 +0800

    Add timeup and applause sound effects.

--- services/sound_effects_service.gd
+++ services/sound_effects_service.gd
@@ -6,21 +6,37 @@ extends Node
 @onready var click: AudioStreamPlayer = $Click
 @onready var count: AudioStreamPlayer = $Count
 @onready var go: AudioStreamPlayer = $Go
+@onready var timeup: AudioStreamPlayer = $Timeup
+@onready var applause: AudioStreamPlayer = $Applause
+
 
 func play_hit():
 	hit.play()
 
+
 func play_success():
 	success.play()
+
 
 func play_decoy():
 	decoy.play()
 
+
 func play_click():
 	click.play()
+
 
 func play_count():
 	count.play()
 
+
 func play_go():
 	go.play()
+
+
+func play_timeup():
+	timeup.play()
+
+
+func play_applause():
+	applause.play()
