73f06a13abe87bfe09f7b7778897906d14b0f5d3
Christopher Perry <ozbonus@gmail.com>
2025-11-16 18:35:57 +0800

    Add more sound effects.

--- services/sound_effects_service.gd
+++ services/sound_effects_service.gd
@@ -3,6 +3,9 @@ extends Node
 @onready var hit: AudioStreamPlayer = $Hit
 @onready var success: AudioStreamPlayer = $Success
 @onready var decoy: AudioStreamPlayer = $Decoy
+@onready var click: AudioStreamPlayer = $Click
+@onready var count: AudioStreamPlayer = $Count
+@onready var go: AudioStreamPlayer = $Go
 
 func play_hit():
 	hit.play()
@@ -12,3 +15,12 @@ func play_success():
 
 func play_decoy():
 	decoy.play()
+
+func play_click():
+	click.play()
+
+func play_count():
+	count.play()
+
+func play_go():
+	count.play()
