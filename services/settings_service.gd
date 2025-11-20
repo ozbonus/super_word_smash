53c98f7c18dc29f5282521685a0b49893aba1b22
Christopher Perry <ozbonus@gmail.com>
2025-11-20 12:22:07 +0800

    Set default time to five minutes.

--- services/settings_service.gd
+++ services/settings_service.gd
@@ -8,4 +8,4 @@ const LENGTH_STEP = 10.0
 
 @export_group("Default settings")
 @export var mute_audio := false
-@export_range(LENGTH_MIN, LENGTH_MAX, LENGTH_STEP, "suffix:seconds") var game_length := 60.0
+@export_range(LENGTH_MIN, LENGTH_MAX, LENGTH_STEP, "suffix:seconds") var game_length := 300.0
